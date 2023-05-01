4645c20b59fb0bcad2e914dc1a49eade1ca9ebe5
potter <potter0409@outlook.com>
2023-05-01 17:51:13 +0000

    Initial commit

---
+++ application/logs/log-2022-12-29.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-12-29 21:56:09 --> Severity: error --> Exception: Undefined constant "CURLOPT_RETURNTRANSFER" /home/vlblum/domains/panel.vlblum.de/public_html/application/libraries/sms/App_sms.php 44
+ERROR - 2022-12-29 21:58:32 --> Severity: error --> Exception: Undefined constant "CURLOPT_RETURNTRANSFER" /home/vlblum/domains/panel.vlblum.de/public_html/application/libraries/sms/App_sms.php 44
+ERROR - 2022-12-29 21:58:46 --> Severity: error --> Exception: Undefined constant "CURLOPT_RETURNTRANSFER" /home/vlblum/domains/panel.vlblum.de/public_html/application/libraries/sms/App_sms.php 44
+ERROR - 2022-12-29 20:59:18 --> 404 Page Not Found: /index
+ERROR - 2022-12-29 20:59:18 --> 404 Page Not Found: /index
